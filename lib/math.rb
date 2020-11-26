6655314c53ab8068c0f96f9255a8cf8627cb1098
Max Miller <maxmiller413@gmail.com>
2020-11-26 07:22:20 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,35 @@
 def addition(num1, num2)
-
+  addition = (num1 + num2)
+  puts addition
+  addition
 end
 
 def subtraction(num1, num2)
-
+  subtraction = (num1 - num2)
+  puts subtraction
+  subtraction
 end
 
 def division(num1, num2)
-
+  division = (num1 / num2)
+  puts division
+  division
 end
 
 def multiplication(num1, num2)
-
+  multiplication = (num1 * num2)
+  puts multiplication
+  multiplication
 end
 
 def modulo(num1, num2)
-
+  modulo = (num1 % num2)
+  puts modulo
+  modulo
 end
 
 def square_root(num)
-
+  square_root = Math.sqrt(num)
+  puts square_root
+  square_root
 end

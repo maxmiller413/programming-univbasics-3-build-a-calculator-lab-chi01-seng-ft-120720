a5336f99ff675c2d16c4cba14028f9a89594f7b6
Max Miller <maxmiller413@gmail.com>
2020-11-26 07:05:09 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -5,21 +5,31 @@ def addition(num1, num2)
 end
 
 def subtraction(num1, num2)
-  puts (num1 - num2)
+  subtration = (num1 - num2)
+  puts subtration
+  subtraction
 end
 
 def division(num1, num2)
-  puts (num1 / num2)
+  division = (num1 / num2)
+  puts division
+  division
 end
 
 def multiplication(num1, num2)
-  puts (num1 * num2)
+  multiplicaiton = (num1 * num2)
+  puts multiplication
+  multiplication
 end
 
 def modulo(num1, num2)
-  puts (num1 % num2)
+  modulo = (num1 % num2)
+  puts modulo
+  modulo
 end
 
 def square_root(num)
-  puts sqrt(num)
+  square_root = sqrt(num)
+  puts square_root
+  square_root
 end
